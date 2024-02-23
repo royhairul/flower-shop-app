b7768bb5fa119d4c48529af3e30d1eb17419a09a
royhairul <royhairulanam19@gmail.com>
2024-02-23 20:52:07 +0700

    Setting main seeder for running UserSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,7 +12,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        $this->call(FlowerSeeder::class);
+        $this->call([
+            FlowerSeeder::class,
+            UserSeeder::class
+        ]);
         // \App\Models\User::factory(10)->create();
 
         // \App\Models\User::factory()->create([
