bbf17b20a1ae6d1ebb7063e7314555a014ed292f
royhairul <royhairulanam19@gmail.com>
2024-02-23 20:53:54 +0700

    Change create to insert, and Add sample data

--- database/seeders/FlowerSeeder.php
+++ database/seeders/FlowerSeeder.php
@@ -14,11 +14,30 @@ class FlowerSeeder extends Seeder
      */
     public function run(): void
     {
-        Flower::create([
-            'name' => 'Mawar Merah',
-            'price' => 10000,
-            'stock' => 10,
-            'category' => 'Bunga Potong'
-        ]);
+        $dataFlowers = [
+            [
+                'image' => 'https://i.pinimg.com/736x/f1/61/7f/f1617f0c33b4c1c5b8a997167ce00218.jpg',
+                'name' => 'Red Rose',
+                'price' => 10000,
+                'stock' => 10,
+                'category' => 'Wedding Decoration'
+            ],
+            [
+                'image' => 'https://ak1.ostkcdn.com/images/products/is/images/direct/04d262c6e48ceb2bc1e8f32562213f61179d4623/Enova-Home-Large-Artificial-Real-Touch-White-Tulip-Arrangement-in-Clear-Glass-Vase-for-Home-Office-Decoration.jpg?impolicy=medium',
+                'name' => 'White Tulip',
+                'price' => 15000,
+                'stock' => 20,
+                'category' => 'Home Decoration'
+            ],
+            [
+                'image' => 'https://images.tokopedia.net/img/cache/700/VqbcmM/2022/12/19/4a680550-5faf-4381-93dd-4608c21479c4.jpg',
+                'name' => 'Spring Floral',
+                'price' => 20000,
+                'stock' => 25,
+                'category' => 'Bouquet Flower'
+            ]
+        ];
+
+        Flower::insert($dataFlowers);
     }
 }
